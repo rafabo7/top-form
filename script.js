4533c5079d06479960fa7947050d97f36ba16d9f
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-03-14 10:07:55 +0100

    At last... After correcting very very silly mistakes I managed to display when an input is focusout empty. The mistake: I didn't add the 'defer' attribute to my script tag!!! Plus: increase the div.logo opacity and the form labels font-size for better readability.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const fields = document.querySelectorAll('.field')
 
+function checkInput(e) {
+   const target = e.target
+   const field = target.closest('.field')
+   console.log(target)
+
+    if (target.value === "") {
+        field.classList.add('error')
+    } else {
+        field.classList.remove('error')
+    }
+}
+
+fields.forEach(e => e.addEventListener('focusout', (e) => checkInput(e)))
