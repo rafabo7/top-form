ad4cbb7f00c42da7319474af2d8de2c19e927964
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-03-15 10:19:16 +0100

    Add checks for the password Add a function to check if the passwords fiels contain the same value, if not the error message is displayed. Followed the same method that the other inputs. It was necessary to add more specific class to the fields.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const fields = document.querySelectorAll('.field')
+const fields = document.querySelectorAll('.field.control')
+const psFields = document.querySelectorAll('.field.pw')
+
+const pws = document.querySelectorAll('[type="password"]')
 
 function checkInput(e) {
    const target = e.target
    const field = target.closest('.field')
-   console.log(target)
 
-    if (target.value === "") {
+   if (target.type == "password") {
+    checkPassword(target, field)
+   } else if (target.value === "") {
         field.classList.add('error')
     } else {
         field.classList.remove('error')
@@ -13,3 +17,14 @@ function checkInput(e) {
 }
 
 fields.forEach(e => e.addEventListener('focusout', (e) => checkInput(e)))
+
+pws.forEach(e => e.addEventListener('change', () => {
+    const pw1 = document.querySelector('#password-1')
+    const pw2 = document.querySelector('#password-2')
+
+    if (pw1.value != pw2.value) {
+        psFields.forEach(e => e.classList.add('error'))
+    } else {
+        psFields.forEach(e => e.classList.remove('error'))
+    }
+}))
